3d2f3af7498667f1ff9ba2d0a383183c156c2ed8
RoanKathrina <roan.j.dimaculangan@accenture.com>
2020-02-27 18:40:37 +0800

    Updated to create leave-management-system.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [
+                  BrowserModule,
+                  FormsModule,
+                  AppRoutingModule,
+                  ReactiveFormsModule
+                ],
+  declarations: [ AppComponent, LoginPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
