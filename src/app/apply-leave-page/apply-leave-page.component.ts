8433941b006c3f1b427615a9f6a68f5653e36071
RoanKathrina <roan.j.dimaculangan@accenture.com>
2020-03-02 19:09:27 +0800

    Updated to create apply-leave-page.

--- src/app/apply-leave-page/apply-leave-page.component.ts
+++ src/app/apply-leave-page/apply-leave-page.component.ts
@@ -9,7 +9,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ApplyLeavePageComponent implements OnInit {
 
   applyLeaveForm: FormGroup;
-  type_of_leave = ['Sick', 'Vacation', 'Maternity']
+  type_of_leave = ['Sick', 'Vacation', 'Maternity'];
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  years = ['2020', '2021', '2022', '2023', '2024', '2025'];
+
   constructor() { }
 
   ngOnInit() {
@@ -20,7 +23,28 @@ export class ApplyLeavePageComponent implements OnInit {
     this.applyLeaveForm = new FormGroup({
       'firstName': new FormControl(null, Validators.required),
       'lastName': new FormControl(null, Validators.required),
-      'typeOfLeave': new FormControl(this.type_of_leave[0], Validators.required)
+      'typeOfLeave': new FormControl(this.type_of_leave[1], Validators.required),
+      'fromMonth': new FormControl(this.months[0], Validators.required),
+      'fromDay': new FormControl('1', Validators.required),
+      'fromYear': new FormControl(this.years[0], Validators.required),
+      'toMonth': new FormControl(this.months[0], Validators.required),
+      'toDay': new FormControl('1', Validators.required),
+      'toYear': new FormControl(this.years[0], Validators.required),
+      'numberOfDays': new FormControl(null)
     })
+  }
+  onSubmit() {
+    console.log(this.applyLeaveForm.get('fromMonth').value);
+    console.log(this.applyLeaveForm.get('fromDay').value);
+    console.log(this.applyLeaveForm.get('fromYear').value);
+  }
+
+  calculateNumberOfDays() {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(parseInt(this.applyLeaveForm.get('toYear').value), parseInt(this.applyLeaveForm.get('toMonth').value), parseInt(this.applyLeaveForm.get('toDay').value)); // To Date
+    const secondDate = new Date(parseInt(this.applyLeaveForm.get('fromYear').value), parseInt(this.applyLeaveForm.get('fromMonth').value), parseInt(this.applyLeaveForm.get('fromDay').value)); // From Date
+    const diffDays = Math.abs((firstDate.valueOf() - secondDate.valueOf())/(24*60*60*1000)) + 1;; 
+    //Math.floor(firstDate.getTime() / (3600 * 24 * 1000)) - Math.floor(secondDate.getTime() / (3600 * 24 * 1000)) + 1;
+    console.log(diffDays);
   }
 }
